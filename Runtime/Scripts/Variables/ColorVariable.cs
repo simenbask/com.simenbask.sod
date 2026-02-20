e0278386e4735b4b29172b4083aac53d6e66b73e
Simen <simskje@gmail.com>
2026-02-20 16:47:21 +0100

    feat: add color variable

---
+++ Runtime/Scripts/Variables/ColorVariable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace com.simenbask.sod.Runtime
+{
+    [CreateAssetMenu(fileName = "New Color Variable", menuName = "ScriptableObject Data/Variables/Create new color var")]
+    public class ColorVariable : Variable<Color>
+    {
+    }
+}
