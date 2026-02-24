f810ea752d9f997281a7de74870cdd97c162d434
Simen <simskje@gmail.com>
2026-02-24 14:16:53 +0100

    fix: remove double serialization
    todo: create custom Editor for stringvar to add textarea attribute

--- Runtime/Scripts/Variables/StringVariable.cs
+++ Runtime/Scripts/Variables/StringVariable.cs
@@ -5,7 +5,5 @@ namespace com.simenbask.sod.Runtime
     [CreateAssetMenu(fileName = "New String Variable", menuName = "ScriptableObject Data/Variables/Create new string var")]
     public class StringVariable : Variable<string>
     {
-        [SerializeField, TextArea(5,20)]
-        new protected string _value;
     }
 }
